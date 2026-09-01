7565803ea2b31d7b14f42924b2eb14931bdc6072
Philemon Kasereka <kmboghaphilemon@gmail.com>
2026-09-01 15:17:21 +0300

    feat(9.10): install cors type definitions and resolve TS errors

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,10 +1,14 @@
 import express, { type Request, type Response } from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = 3001;
 
+// Global middleware
+app.use(cors());
 app.use(express.json());
 
+// Health check endpoint
 app.get("/api/ping", (_req: Request, res: Response) => {
   console.log("someone pinged here");
   res.send("pong");
